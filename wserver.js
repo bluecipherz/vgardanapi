8d00de83d45a028d55e8c0cee868828617c3957a
rinasm <wings@fire99>
2018-02-17 13:44:57 +0530

    wsever error fixed

--- wserver.js
+++ wserver.js
@@ -135,7 +135,7 @@ http.listen(appconfig.wsport, function(){
     setInterval(()=>{
         checkStatus();
         sendToMQTT(commandMap.COMMON.REQSTATUS);
-    }, 2000);
+    }, 10000);
 });
 
 app.post('/', function (req, res) {
